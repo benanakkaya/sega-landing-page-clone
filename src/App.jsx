7da172f79090053777244030e48c5dcb5d970b73
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-05-19 19:09:30 +0300

    footer and responsive missing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import '../dist/css/main.css'
+import Content from './components/Content'
+import FollowUs from './components/FollowUs'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+   <div>
+    <Header/>
+    <Content />
+    <FollowUs />
+   </div>
+  )
+}
+
+export default App
